f38f5ca1d8dea14bf9ea1c6d6d14a9c364ef743e
viktor-rak-laptop <vrakov2012@gmail.com>
2025-02-28 10:31:09 -0500

    im a stupid dumbfuck but its done

--- initial-utilities/wgrep/wgrep.c
+++ initial-utilities/wgrep/wgrep.c
@@ -17,8 +17,8 @@ int main(int argc, char* argv[]) {
     char buf[256];
     char* line = NULL;
     ssize_t l_read;
-    ssize_t len;
-    char* tgt = malloc(sizeof(argv[1]) + 1);
+    ssize_t len = 0;
+    char* tgt = malloc(strlen(argv[1]) + 1);
     strcpy(tgt, argv[1]);
 
     if (argc == 2) {
@@ -27,16 +27,18 @@ int main(int argc, char* argv[]) {
                 printf("%s", buf);
             }
         }
+        return 0;
         //TODO: now read with fgets and use similar comparison logic
         //maybe make a function for strstr logic.
         //at this point i just need to handle stdout now
     }
 
     for (size_t i = 2; i < argc; i++) {
-        char* fname = malloc(sizeof(argv[i]) + 1);
+        char* fname = malloc(strlen(argv[i]) + 1);
         strcpy(fname, argv[i]);
         if ((file = fopen(fname, "r")) == NULL) {
             printf("wgrep: cannot open file\n");
+            free(fname);
             return 1;
         }
         while ((l_read = getline(&line, &len, file)) != -1) {
@@ -45,6 +47,7 @@ int main(int argc, char* argv[]) {
             }
         }
         free(fname);
+        line = NULL; //need to do this to not reuse the pointer
     }
 
     return 0;
